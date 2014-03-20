4e60d9cd3291ce80a1aea882a5598e9c5c5ddafb
Ein Verne <einverne@gmail.com>
2014-03-20 14:34:40 +0800

    make points string to lua

--- Classes/HcharacterLayer.cpp
+++ Classes/HcharacterLayer.cpp
@@ -34,6 +34,34 @@ HcharacterLayer* HcharacterLayer::create(CCSprite* tianzige_draw){
 	}
 }
 
+void HcharacterLayer::judge(vector<CCPoint> points){
+
+	string output = "";
+	for (vector<CCPoint>::iterator iter = points.begin(); iter != points.end() ; ++iter)
+	{
+		CCPoint temp = *iter;
+		temp = m_sprite_draw->convertToNodeSpace(temp);
+		temp = convert512(temp);
+		string t = floatToString(ceil(temp.x)) + "/" + floatToString(ceil(temp.y)) + "/";
+		output += t;	
+	}
+	output += "@";
+	
+}
+
+CCPoint HcharacterLayer::convert512(CCPoint p){
+	CCSize size = this->m_sprite_draw->getContentSize();
+	float fx = p.x * 512 / size.width;
+	float fy = p.y * 512 / size.height;
+	return ccp(fx,-(fy-512));
+}
+
+string HcharacterLayer::floatToString(float f){
+	ostringstream buff;
+	buff<<f;
+	return buff.str();
+}
+
 void HcharacterLayer::onEnter(){
 
 }
--- Classes/HcharacterLayer.h
+++ Classes/HcharacterLayer.h
@@ -15,6 +15,9 @@ public:
 	virtual bool init(CCSprite* tianzige_draw);
 	virtual void onExit();
 	virtual void onEnter();
+	CCPoint convert512(CCPoint p);
+	string floatToString(float f);
+	void judge(vector<CCPoint> points);
 
 	HcharacterDrawnode* m_HDrawnode;
 
