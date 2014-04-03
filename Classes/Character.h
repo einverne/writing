4718ef522dcc810b0eb2a7de8c59ecf2d9052237
Ein Verne <einverne@gmail.com>
2014-04-03 14:47:13 +0800

    document Character

--- Classes/Character.h
+++ Classes/Character.h
@@ -10,12 +10,34 @@ class Character
 {
 public:
 	bool addBujian(Bujian bujian);
+	//************************************
+	// Method:    getBox 获取包围盒
+	// FullName:  Character::getBox
+	// Access:    public 
+	// Returns:   cocos2d::CCSize
+	// Qualifier:
+	//************************************
 	CCSize getBox();
+	//************************************
+	// Method:    getStrokeCount 获取字的笔画数
+	// FullName:  Character::getStrokeCount
+	// Access:    public 
+	// Returns:   int
+	// Qualifier:
+	//************************************
 	int getStrokeCount();
 	void resize(CCSize size);
 	void resample();
 	void prepareDrawNode();
-	Stroke getStroke(int no);			//获取第几笔
+	//************************************
+	// Method:    getStroke 获取第no笔，笔画从1开始
+	// FullName:  Character::getStroke
+	// Access:    public 
+	// Returns:   Stroke
+	// Qualifier:
+	// Parameter: int no
+	//************************************
+	Stroke getStroke(int no);
 public:
 	Character(void);
 	~Character(void);
