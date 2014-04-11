d8a99c1f3ee56833bbd2318e94336972cc960c8a
Ein Verne <einverne@gmail.com>
2014-04-11 19:37:09 +0800

    fix android.mk

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -7,14 +7,36 @@ LOCAL_MODULE := cocos2dcpp_shared
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
+				   ../../Classes/sqlite3.c \
                    ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp \
-                   ../../Classes/GameOverScene.cpp \
-                   ../../Classes/GameScene.cpp \
-                   ../../Classes/SecondScene.cpp \
-                   ../../Classes/TestMoveSprite.cpp\
-                   ../../Classes/HclcData.cpp\
-                   ../../Classes/GameWall.cpp\
+                   ../../Classes/BackgroundLayer.cpp \
+                   ../../Classes/Bujian.cpp \
+                   ../../Classes/Character.cpp \
+                   ../../Classes/HanziManage.cpp \
+                   ../../Classes/HcharacterDrawnode.cpp \
+                   ../../Classes/HcharacterLayer.cpp \
+                   ../../Classes/HclcData.cpp \
+                   ../../Classes/JudgeManager.cpp \
+                   ../../Classes/LianxiScene.cpp \
+                   ../../Classes/LuaScriptReader.cpp \
+                   ../../Classes/PopLayer.cpp \
+                   ../../Classes/ReadXML.cpp \
+                   ../../Classes/Splash.cpp \
+                   ../../Classes/Stroke.cpp \
+                   ../../Classes/StrokeDrawnode.cpp \
+                   ../../Classes/StrokeNode.cpp \
+                   ../../Classes/TcharacterDrawnode.cpp \
+                   ../../Classes/TcharacterLayer.cpp \
+                   ../../Classes/Tianzige.cpp \
+                   ../../Classes/TouchLayer.cpp \
+                   ../../Classes/WallScene.cpp \
+                   ../../Classes/tinystr.cpp \
+                   ../../Classes/tinyxml.cpp \
+                   ../../Classes/tinyxmlerror.cpp \
+                   ../../Classes/tinyxmlparser.cpp \
+                   ../../Classes/MoveToRightPlaceInterval.cpp \
+                   ../../Classes/SqliteHelper.cpp \
+                   ../../Classes/CharacterEntity.cpp \
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes\
 
@@ -24,11 +46,12 @@ LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
 LOCAL_WHOLE_STATIC_LIBRARIES += chipmunk_static
 LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
 LOCAL_WHOLE_STATIC_LIBRARIES += luajit_static
-
 LOCAL_WHOLE_STATIC_LIBRARIES += cocos_lua_static
 
 include $(BUILD_SHARED_LIBRARY)
 
+$(call import-add-path,D:\graduation_project\cocos2d-x-2.1.5)
+$(call import-add-path,D:\graduation_project\cocos2d-x-2.1.5\cocos2dx\platform\third_party\android\prebuilt\)
 $(call import-module,cocos2dx)
 $(call import-module,cocos2dx/platform/third_party/android/prebuilt/libcurl)
 $(call import-module,CocosDenshion/android)
