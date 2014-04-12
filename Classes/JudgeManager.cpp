140d7f16626a815f1cbcc7ea828fe158bd8615bb
Ein Verne <einverne@gmail.com>
2014-04-12 22:23:15 +0800

    add android support

--- Classes/JudgeManager.cpp
+++ Classes/JudgeManager.cpp
@@ -12,7 +12,8 @@ JudgeManager::~JudgeManager()
 	CCLog("JudgeManager destroy!");
 }
 string JudgeManager::getResult(string points_output){
-	string dbpath = CCFileUtils::sharedFileUtils()->fullPathForFilename("test.db");
+//	string dbpath = CCFileUtils::sharedFileUtils()->fullPathForFilename("test.db");
+	string dbpath = CCFileUtils::sharedFileUtils()->getWritablePath()+"test.db";
 	SqliteHelper::initDB(dbpath.c_str());
 	string sql = "select * from table_test where name='"+hanzi+"'";
 	CharacterEntity* p = new CharacterEntity();
@@ -21,6 +22,7 @@ string JudgeManager::getResult(string points_output){
 	stringstream ss;
 	ss << p->getID()->getValue();
 	string path = "lua/ZiList/"+ ss.str();
+	CCLog("path %s",path.c_str());
 
 	string filepath = CCFileUtils::sharedFileUtils()->fullPathForFilename("lua/WriteZiInfo.lua");
 	string Globalpath = CCFileUtils::sharedFileUtils()->fullPathForFilename(string(path+"/funcs.txt").c_str());
@@ -29,10 +31,11 @@ string JudgeManager::getResult(string points_output){
 	string rulespath = CCFileUtils::sharedFileUtils()->fullPathForFilename(string(path+"/rules.txt").c_str());
 
 	gReader.InitLuaScriptReader();
-	char* temp = new char[points_output.length() + 1];
-	strcpy(temp,points_output.c_str());
-	gReader.GetWriteZiInfo(temp);
-	delete [] temp;
+//	char* temp = new char[points_output.length() + 1];
+//	strcpy(temp,points_output.c_str());
+//	gReader.GetWriteZiInfo(temp);
+//	delete [] temp;
+	gReader.GetWriteZiInfo(points_output.c_str());
  	char * retStr = new char[50];
  	gReader.GetZiName(hanzi);
  	gReader.SetRulesFunc(rulespath.c_str());
@@ -41,6 +44,7 @@ string JudgeManager::getResult(string points_output){
  	gReader.RunMixedFile(basepath.c_str(),"BaseLib.lua");
  	gReader.RunScriptFile(apipath.c_str(),retStr,"RunAPI.lua");
  	gReader.ExitLuaScriptReader();
+ 	CCLog("retStr %s",retStr);
 	string ret(retStr);
 	delete [] retStr;
 	return ret;
