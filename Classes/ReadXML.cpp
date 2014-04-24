e67662067070fbb3ba25fde9c3ca89f5c42267ff
Ein Verne <einverne@gmail.com>
2014-04-24 13:25:50 +0800

    document ReadXML class

--- Classes/ReadXML.cpp
+++ Classes/ReadXML.cpp
@@ -7,8 +7,27 @@
 USING_NS_CC;
 using namespace std;
 
-CReadXML::CReadXML(void)
+CReadXML::CReadXML(const char* xmlcontent)
 {
+	TiXmlDocument* document = new TiXmlDocument();
+	document->Parse(xmlcontent,0,TIXML_ENCODING_UTF8);
+	TiXmlElement* rootElement = document->RootElement();
+	TiXmlElement* outlineElement = rootElement->FirstChildElement();
+	TiXmlElement* strokeElement = outlineElement->FirstChildElement();
+	Bujian bujian;
+	for (strokeElement;strokeElement;strokeElement = strokeElement->NextSiblingElement())
+	{
+		TiXmlElement* trunpoint = strokeElement->FirstChildElement();
+		Stroke stroke;
+		for (trunpoint;trunpoint;trunpoint = trunpoint->NextSiblingElement())
+		{
+			const char* x = trunpoint->Attribute("x");
+			const char* y = trunpoint->Attribute("y");
+			stroke.addPoint(ccp(atof(x),atof(y)));
+		}
+		bujian.addStroke(stroke);
+	}
+	character.addBujian(bujian);
 }
 
 
