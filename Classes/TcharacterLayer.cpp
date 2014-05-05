95d15c74f0615c3ec684a20ee273a94147228ba6
Ein Verne <einverne@gmail.com>
2014-05-05 16:31:34 +0800

    annotation two button

--- Classes/TcharacterLayer.cpp
+++ Classes/TcharacterLayer.cpp
@@ -23,19 +23,19 @@ bool TcharacterLayer::init(CCSprite* tianzige){
 // 		dog->setPosition(CCPointMake(300,300));
 // 		this->addChild(dog,1000,1000);
 
-		CCPoint tiangzige_position = this->getSprite()->getPosition();
-		CCSize tianzige_size = this->getSprite()->getContentSize();
-
-		CCMenuItemImage* refreshButton = CCMenuItemImage::create("replay.png",
-			"replay.png",
-			this,
-			menu_selector(TcharacterLayer::refresh));
-		CCPoint refresh_position = ccp(tiangzige_position.x+tianzige_size.width/2+20+refreshButton->getContentSize().width/2,tiangzige_position.y+tianzige_size.height/2-refreshButton->getContentSize().height/2);
-		refreshButton->setPosition(refresh_position);
-
-		CCMenu* menu = CCMenu::create(refreshButton,NULL);
-		menu->setPosition(0,0);
-		this->addChild(menu,2000);
+// 		CCPoint tiangzige_position = this->getSprite()->getPosition();
+// 		CCSize tianzige_size = this->getSprite()->getContentSize();
+// 
+// 		CCMenuItemImage* refreshButton = CCMenuItemImage::create("replay.png",
+// 			"replay.png",
+// 			this,
+// 			menu_selector(TcharacterLayer::refresh));
+// 		CCPoint refresh_position = ccp(tiangzige_position.x+tianzige_size.width/2+20+refreshButton->getContentSize().width/2,tiangzige_position.y+tianzige_size.height/2-refreshButton->getContentSize().height/2);
+// 		refreshButton->setPosition(refresh_position);
+// 
+// 		CCMenu* menu = CCMenu::create(refreshButton,NULL);
+// 		menu->setPosition(0,0);
+// 		this->addChild(menu,2000);
 		return true;
 	}
 	return false;
