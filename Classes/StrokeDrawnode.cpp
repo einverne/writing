6f74a75a2f8dc148910dd59b6fe867d5a03f11f5
Ein Verne <einverne@gmail.com>
2014-05-28 12:07:17 +0800

    add gl function

--- Classes/StrokeDrawnode.cpp
+++ Classes/StrokeDrawnode.cpp
@@ -37,8 +37,11 @@ StrokeDrawnode::~StrokeDrawnode()
 void StrokeDrawnode::draw(){
 	//CCLog("StrokeDrawnode::draw");
 	//在这里设定笔画的粗细和颜色。 具体要查看
-	glLineWidth(6.0f);					//笔画粗细
+	glLineWidth(10.0f);					//笔画粗细
 	ccDrawColor4F(0,0,0,1);				//笔画颜色
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+//	glHint(GL_POINT_SMOOTH_HINT,GL_NICEST);
 // 	glEnable(GL_LINE_SMOOTH);
 	if (visibleIndex == -1)
 	{
