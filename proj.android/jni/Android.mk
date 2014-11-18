ab016523c3f4ac927d5395da63a52f11f97aa238
Ein Verne <einverne@gmail.com>
2014-11-18 23:31:34 +0800

    update android project

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -20,6 +20,7 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../Classes/LianxiScene.cpp \
                    ../../Classes/LuaScriptReader.cpp \
                    ../../Classes/MainScene.cpp \
+                   ../../Classes/MyToast.cpp \
                    ../../Classes/PopLayer.cpp \
                    ../../Classes/ReadXML.cpp \
                    ../../Classes/Splash.cpp \
@@ -54,8 +55,8 @@ LOCAL_WHOLE_STATIC_LIBRARIES += cocos_lua_static
 
 include $(BUILD_SHARED_LIBRARY)
 
-$(call import-add-path,D:\graduation_project\cocos2d-x-2.1.5)
-$(call import-add-path,D:\graduation_project\cocos2d-x-2.1.5\cocos2dx\platform\third_party\android\prebuilt\)
+$(call import-add-path,E:\graduation_project\cocos2d-x-2.1.5)
+$(call import-add-path,E:\graduation_project\cocos2d-x-2.1.5\cocos2dx\platform\third_party\android\prebuilt\)
 $(call import-module,cocos2dx)
 $(call import-module,cocos2dx/platform/third_party/android/prebuilt/libcurl)
 $(call import-module,CocosDenshion/android)
